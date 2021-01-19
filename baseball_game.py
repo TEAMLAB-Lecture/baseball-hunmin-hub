f6ef0dd83c802951b9f6e347f147f1df475ce448
hunmin-hub <hakddal707@naver.com>
2021-01-19 17:55:04 +0900

    Update baseball_game.py

--- baseball_game.py
+++ baseball_game.py
@@ -282,6 +282,9 @@ def main():
         # 위의 코드를 포함하여 자유로운 수정이 가능함
         while True :
             user_input = input('Input guess number : ')
+            if user_input=='' :
+                print('Wrong Input, Input again')
+                continue
             if user_input == '0' :
                 flag=False
                 break
